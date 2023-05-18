d3682eaa27099357e5782f5b0522066df57b1196
deankolev6 <114641015+deankolev6@users.noreply.github.com>
2023-05-18 10:12:46 +0300

    Add files via upload

---
+++ 9 zadacha.sql
@@ -0,0 +1,3 @@
+SELECT `first_name`,`last_name`,`salary`
+from employees
+WHERE `salary` BETWEEN 20000 AND 30000
---
+++ EMAIL sql.sql
@@ -0,0 +1,2 @@
+SELECT CONCAT (`first_name`,'.',`last_name`,'@softuni.bg') AS 'full_email_address'
+from employees
